4b1b41d801908d3b3977c9d9e2b601dda3baa64c
Joel Santos <joeltere9@gmail.com>
2025-02-04 11:28:14 -0300

    first commit

---
+++ src/main/java/com/joel/br/gestao_campeonatos/GestaoCampeonatosApplication.java
@@ -0,0 +1,13 @@
+package com.joel.br.gestao_campeonatos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestaoCampeonatosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestaoCampeonatosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/joel/br/gestao_campeonatos/GestaoCampeonatosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joel.br.gestao_campeonatos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaoCampeonatosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
